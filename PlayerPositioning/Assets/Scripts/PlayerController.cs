0ef28402403b1ef7c9add673f8aaa377e277fb04
Evan Brotzman <brotzmane@roverkids.org>
2023-12-01 14:14:49 -0500

    2.2 half done

--- PlayerPositioning/Assets/Scripts/PlayerController.cs
+++ PlayerPositioning/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,9 +28,22 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-
+        
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        
     }
+         
+    
+        
+    
+
+
 }
+
